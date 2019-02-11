32868e0a83a9a686c0531b415d619feb4908f02f
Abdallah Aboelela <akaboelela@gyarados.cs.uchicago.edu>
2019-02-11 16:02:50 -0600

    more progress on rt_scraper, about to start SQL for it

--- rt_scraper/scraper.py
+++ rt_scraper/scraper.py
@@ -16,39 +16,45 @@ import json
 import sys
 import csv
 
-def get_movie_links(start = 1):
+def get_movie_links():
     '''
     Gets all movie links, short synopsis, runtime, three main actors, and id
     '''
     movie_dict = {}
 
-    start_url = ("https://www.rottentomatoes.com/api/private/v2.0/browse?"
-                 "maxTomato=100&maxPopcorn=100&services=amazon;hbo_go;itunes;"
-                 "netflix_iw;vudu;amazon_prime;fandango_now&certified&sortBy="
-                 "release&type=dvd-streaming-all&page=")
+    start_url = ('https://www.rottentomatoes.com/api/private/v2.0/browse?'
+                 'maxTomato=100&maxPopcorn=100&services=amazon;hbo_go;itunes;'
+                 'netflix_iw;vudu;amazon_prime;fandango_now&certified&sortBy='
+                 'release&type=dvd-streaming-all&page=')
 
-    for i in range(start, 312):
+    for i in range(1, 312):
         r = util.get_request(start_url + str(i))
         result = r.json()
 
-        for movie in result.get("results"):
+        for movie in result.get('results'):
             new_movie = {}
 
-            print(i, movie.get("title"))
-            movie_id = movie.get("id")
+            print(i, movie.get('title'))
+            movie_id = movie.get('id')
 
-            new_movie["actors"] = movie.get("actors")
-            new_movie["runtime_h"] = movie.get("runtime")
-            new_movie["short_synopsis"] = movie.get("synopsis")
-            new_movie["title"] = movie.get("title")
-            new_movie["relative_url"] = movie.get('url')
+            new_movie['actors'] = movie.get('actors')
+            new_movie['h_runtime'] = movie.get('runtime')
+            new_movie['short_synopsis'] = movie.get('synopsis')
+            new_movie['title'] = movie.get('title')
+            new_movie['relative_url'] = movie.get('url')
 
             movie_dict[movie_id] = new_movie
 
     return movie_dict
 
-def all_movies_page_csv(start = 1, index_filename):
-    with open
+MOVIE_DICT = get_movie_links()
 
-def go(num_pages_to_crawl, index_filename):
-    pass
+def all_movies_page_csv(start = 1, index_filename):
+    with open(index_filename, 'w') as csvfile:
+    	writer = csv.writer(csvfile, delimiter = '|')
+
+    	for movie_id, movie in MOVIE_DICT.items():
+    		actors = movie.get('actors')
+    		writer.writerow([movie_id, actors[0], actors[1], actors[2], 
+    						 movie.get('h_runtime'), movie.get(short_synopsis),
+    						 moive.get('title'), movie.get('relative_url')])
