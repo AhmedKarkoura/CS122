14f26136e04107d3051f1f4c188476bfabfabfe9
Scarla Pan <lpan1@cs.uchicago.edu>
2019-02-17 15:57:15 -0600

    write csv added

--- oscars/oscars_scraper.py
+++ oscars/oscars_scraper.py
@@ -13,35 +13,46 @@ import sqlite3
 import urllib.request
 import requests
 
+def get_category_urls(url):
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "html5lib")
+    links = soup.find('div', class_= "div-col columns column-width").find_all('a')
+    urls = []
+    for link in links:
+        category_relative_url = link['href']
+        category_url = util.convert_if_relative_url(url, category_relative_url)
+        urls.append(category_url)
 
-BEST_ACTOR_URL = 'https://en.wikipedia.org/wiki/Academy_Award_for_Best_Actor'
-BEST_SUPPORTING_ACTOR_URL = 'https://en.wikipedia.org/wiki/Academy_Award_for_Best_Supporting_Actor'
-BEST_ACTRESS_URL = 'https://en.wikipedia.org/wiki/Academy_Award_for_Best_Actress'
-BEST_SUPPORTING_ACTRESS_URL = 'https://en.wikipedia.org/wiki/Academy_Award_for_Best_Supporting_Actress'
+    return urls
 
-def get_winners(url):
+def get_acting_nominees(url):
+    nominees = []
+    for url in get_category_urls(url)[2:6]:
+        nominees += get_nominees(url) 
+    write_csv(nominees)
+    return nominees
+
+def get_nominees(url):
+    nominees = []
     html = requests.get(url).text
     soup = BeautifulSoup(html, "html5lib")
     table = soup.find('table', class_='wikitable sortable')
     rows = table.find_all('tr')[1:]  
-    nominees = []
     award = ' '.join(url.split('_')[3:])
 
     for index, row in enumerate(rows):
 
         if not row.find_all('td', style= "background-color:#CACCD0; font-weight:bold; padding-left:20%"):
-            nomination_info = [award]
+            nomination_info = []
 
             if row.th:
                 year = row.th.find_all('a')[0].text  
             nomination_info.append(year)
+            nomination_info.append(award)
             row_info = row.find_all('td')
             if row_info:
                 actor = row_info[0].text.strip('\n')
                 nomination_info.append(actor.strip('§^†*[]1234567890()'))
-                if len(row_info) > 2:
-                    movie = row_info[2].text.strip('\n')
-                nomination_info.append(movie)
 
                 winner = False
                 win_test = row_info[0].find_all('img')
@@ -54,70 +65,28 @@ def get_winners(url):
                 elif '†' in actor:
                     winner = True
                 nomination_info.append(winner)
+                
+                if len(row_info) > 2:
+                    movie = row_info[2].text.strip('\n')
+                nomination_info.append(movie)
+
                 nominees.append(nomination_info)
 
     return nominees
 
-''' 
+def write_csv(nominees):
+    with open('acting_nominees.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerows(nominees)
 
-def get_winners():
-
-
-    headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+def get_best_pic(url):
+    nominees = []
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "html5lib")
+    tables = soup.find_all('table', class_='wikitable')
+    for table in tables:
+        rows = table.find_all('tr')[1:]
+        for index, row in enumerate(rows):
+            row_info = row.find_all('td')
     
-    data = {'__RequestVerificationToken': 'utBox4gcI4FksC98fTDpUNkdf_exGrnnRS4idAnCT5xFb2svLAaHYqn_q5CjrWkL9HR8I0qJlyGP308MnMz3ETCgOxQ1',
-            'BasicSearchView_FilmTitle' : '', 
-            'BasicSearchView_Nominee' : '', 
-            'BasicSearchView_IsWinnersOnly' : 'false',
-            'BasicSearchView_AwardShowNumberFrom' : '1',
-            'BasicSearchView_AwardShowNumberTo' : '90',
-            'BasicSearchView_Sort' : '3-Award Category-Chron',
-            'search' : 'Basic',
-            'AdvancedSearchView_SearchFieldFilters[0]_Index' : '1',
-            'AdvancedSearchView_SearchFieldFilters[0]_Field.Id' : '0',
-            'AdvancedSearchView_SearchFieldFilters[0]_FieldValues' : '', 
-            'AdvancedSearchView_SearchFieldFilters[0]_FieldValues' : '',
-            'AdvancedSearchView_SearchFieldFilters[0]_Condition' : '0',
-            'AdvancedSearchView_SearchFieldFilters[0]_SearchType' : '0',
-            'AdvancedSearchView_SearchFieldFilters[0]_IsExcluded' : 'false',
-            'AdvancedSearchView_SearchFieldFilters[1]_Index' : '2',
-            'AdvancedSearchView_SearchFieldFilters[1]_Field.Id' : '0',
-            'AdvancedSearchView_SearchFieldFilters[1]_FieldValues' : '',
-            'AdvancedSearchView_SearchFieldFilters[1]_FieldValues' : '', 
-            'AdvancedSearchView_SearchFieldFilters[1]_Condition' : '0',
-            'AdvancedSearchView_SearchFieldFilters[1]_SearchType' : '0',
-            'AdvancedSearchView_SearchFieldFilters[1]_IsExcluded' : 'false',
-            'AdvancedSearchView_SearchFieldFilters[2]_Index' : '3',
-            'AdvancedSearchView_SearchFieldFilters[2]_Field.Id' : '0',
-            'AdvancedSearchView_SearchFieldFilters[2]_FieldValues' : '',
-            'AdvancedSearchView_SearchFieldFilters[2]_FieldValues' : '',
-            'AdvancedSearchView_SearchFieldFilters[2]_SearchType' : '0',
-            'AdvancedSearchView_SearchFieldFilters[2]_IsExcluded' : 'false',
-            'AdvancedSearchView_IsWinnersOnly' : 'false',
-            'AdvancedSearchView_AwardShowNumberFrom' : '0',
-            'AdvancedSearchView_AwardShowNumberTo' : '0', 
-            'AdvancedSearchView_IsWomenOnly' : 'false',
-            'AdvancedSearchView_IsMenOnly' : 'false',
-            'AdvancedSearchView_IsDebut' : 'false',
-            'AdvancedSearchView_IsPosthumous' : 'false',
-            'AdvancedSearchView_IsShort' : 'false',
-            'AdvancedSearchView_IsFeature' : 'false',
-            'AdvancedSearchView_IsForeignLanguage' : 'false',
-            'AdvancedSearchView_Sort' : '3-Award Category-Chron'}
-
-    starting_url = 'http://awardsdatabase.oscars.org/search/results'
-    r = requests.post(url = starting_url, json = data, headers = headers)
-    #c = r.content
-    #soup = BeautifulSoup(c)
-    #year_list = soup.find_all('div', class_="result-group-header")
-
-    session = HTMLSession()
-    request = urllib.request.urlopen(starting_url)
-    html = request.read()
-    soup = bs4.BeautifulSoup(html, "html5lib")
-    year_list = soup.find_all('div', class_="result-group-header")
-
-    year_list = soup.find_all('div', class_="award_result-chron result-group group-awardcategory-chron")
-    for year_tag in year_list:
-        year = year_tag.find_all('a', class_="nominations_link")
-'''
+    return tables
