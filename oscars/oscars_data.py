51187f2b6005d2789d855ba0a818ead4a6bcf586
Scarla Pan <lpan1@cs.uchicago.edu>
2019-02-17 23:06:58 -0600

    all awards compiled

--- oscars/oscars_data.py
+++ oscars/oscars_data.py
@@ -1,40 +1,16 @@
 import numpy as np
 import pandas as pd 
 
-CATEGORIES = ['ACTOR', 'ACTRESS', 'ART DIRECTION', 'CINEMATOGRAPHY',
-       'DIRECTING ', 'ENGINEERING EFFECTS', 'OUTSTANDING PICTURE',
-       'UNIQUE AND ARTISTIC PICTURE', 'WRITING ', 'DIRECTING', 'WRITING',
-       'OUTSTANDING PRODUCTION', 'SOUND RECORDING', 'SHORT SUBJECT ',
-       'ASSISTANT DIRECTOR', 'FILM EDITING', 'MUSIC ', 'DANCE DIRECTION',
-       'ACTOR IN A SUPPORTING ROLE', 'ACTRESS IN A SUPPORTING ROLE',
-       'IRVING G. THALBERG MEMORIAL AWARD', 'SPECIAL AWARD',
-       'CINEMATOGRAPHY ', 'SPECIAL EFFECTS',
-       'SCIENTIFIC OR TECHNICAL AWARD ', 'ART DIRECTION ', 'DOCUMENTARY ',
-       'OUTSTANDING MOTION PICTURE', 'DOCUMENTARY', 'BEST MOTION PICTURE',
-       'COSTUME DESIGN ', 'HONORARY FOREIGN LANGUAGE FILM AWARD',
-       'FOREIGN LANGUAGE FILM', 'JEAN HERSHOLT HUMANITARIAN AWARD',
-       'COSTUME DESIGN', 'SOUND', 'BEST PICTURE', 'SOUND EFFECTS',
-       'SPECIAL VISUAL EFFECTS', 'SPECIAL ACHIEVEMENT AWARD ',
-       'SHORT FILM ', 'ACTOR IN A LEADING ROLE',
-       'ACTRESS IN A LEADING ROLE', 'VISUAL EFFECTS', 'MAKEUP',
-       'GORDON E. SAWYER AWARD', 'SOUND EFFECTS EDITING',
-       'AWARD OF COMMENDATION', 'SOUND EDITING', 'ANIMATED FEATURE FILM',
-       'SOUND MIXING', 'MAKEUP AND HAIRSTYLING', 'PRODUCTION DESIGN']
-       
-BEST_PICTURE = ["BEST MOTION PICTURE", "OUTSTANDING MOTION PICTURE", "OUTSTANDING PRODUCTION", "BEST PICTURE"]
-BEST_ACTOR = ["ACTOR", "ACTOR IN A LEADING ROLE", "ACTOR IN A SUPPORTING ROLE"]
-BEST_ACTRESS = ["ACTRESS", "ACTRESS IN A LEADING ROLE", "ACTRESS IN A SUPPORTING ROLE"]
+DISCONTINUED_CATEGORIES = ['SHORT SUBJECT (One-reel)', 'SHORT SUBJECT (Two-reel)', 'ASSISTANT DIRECTOR', 'DANCE DIRECTION', 'ENGINEERING EFFECTS',
+                           'WRITING (Original Story)', 'MUSIC (Scoring of a Musical Picture)','MUSIC (Music Score of a Dramatic or Comedy Picture)',
+                           'SHORT SUBJECT (Color)', 'SHORT SUBJECT (Comedy)', 'SHORT SUBJECT (Novelty)', 'WRITING (Title Writing)', 'ACTOR IN A SUPPORTING ROLE',
+                           'ACTOR IN A LEADING ROLE', 'ACTRESS IN A LEADING ROLE', 'ACTRESS IN A SUPPORTING ROLE', 'ACTOR', 'ACTRESS']
 
-oscars_db = pd.read_csv('oscars.csv')
-oscars_db = oscars_db.loc[oscars_db.entity.str.startswith("To") == False]
+def get_awards():
+   oscars = pd.read_csv('oscars.csv')
+   oscars = oscars.loc[oscars.entity.str.startswith("To") == False]
+   oscars = oscars.loc[oscars.category.str.contains("SPECIAL") == False]
+   oscars = oscars.loc[oscars.category.str.contains("AWARD") == False]
+   oscars = oscars.loc[oscars.category.isin(DISCONTINUED_CATEGORIES) == False]
+   oscars.to_csv(r'oscars_awards.csv', index = False)
 
-best_pic_nominees = oscars_db.loc[oscars_db.category.isin(BEST_PICTURE) == True]
-best_pic_winners = best_pic_nominees.loc[best_pic_nominees.winner == True]
-
-best_actor_nominees = oscars_db.loc[oscars_db.category.isin(BEST_ACTOR) == True]
-best_actor_winners = best_actor_nominees.loc[best_actor_nominees.winner == True]
-
-best_actress_nominees = oscars_db.loc[oscars_db.category.isin(BEST_ACTRESS) == True]
-best_actress_winners = best_actress_nominees.loc[best_actress_nominees.winner == True]
-
-oscars_db['cat'] = oscars_db['category'].str.split('(').str.get(0)
