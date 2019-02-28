9bb271a12cfc3af5509cb35d174470d9417245cc
Scarla Pan <lpan1@cs.uchicago.edu>
2019-02-27 19:47:08 -0600

    filter without oscars

---
+++ sql_filter/create_db.sql
@@ -0,0 +1,56 @@
+CREATE TABLE awards (
+	year integer,
+	category VARCHAR(100),
+	winner boolean,
+	movie VARCHAR(200));
+
+.separator ,
+.import oscars_awards.csv awards
+
+CREATE TABLE acting_nominees(
+	year integer,
+	category VARCHAR(100),
+	actor VARCHAR(100),
+	winner boolean,
+	movie VARCHAR(200));
+.separator ,
+.import acting_nominees.csv acting_nominees
+
+CREATE TABLE ratings (
+    movie_id VARCHAR(50),
+    imdb_score int,
+    title VARCHAR(255),
+    box_office VARCHAR(50),
+    mpaa VARCHAR(1024),
+    runtime int,
+    studio VARCHAR(1024),
+    full_synop VARCHAR(2048),
+    critics_score VARCHAR(32),
+    audience_score int,
+    year int,
+    genre1 VARCHAR(255),
+    genre2 VARCHAR(255),
+    genre3 VARCHAR(255),
+    director1 VARCHAR(255),
+    director2 VARCHAR(255),
+    writer1 VARCHAR(255),
+    writer2 VARCHAR(255),
+    writer3 VARCHAR(255)
+    );
+.separator ,
+.import cleaned_matches.csv ratings
+
+CREATE TABLE principal (
+	movie_id VARCHAR(50),
+	name_id VARCHAR(50),
+	category VARCHAR(255));
+.separator ,
+.import final_principals.csv principal
+
+CREATE TABLE names (
+	name_id VARCHAR(50),
+	name VARCHAR(255),
+	profession VARCHAR(255),
+	known_for VARCHAR(1024));
+.separator ,
+.import final_names.csv names
--- sql_filter/filter.py
+++ sql_filter/filter.py
@@ -1,4 +1,46 @@
 import sqlite3
+import pandas as pd
+import numpy as np
+from fuzzywuzzy import fuzz
+import csv
+
+def clean_csv(csv_file_name):
+    ratings = pd.read_csv(csv_file_name)
+    ratings = ratings[['tconst', 'averageRating', 'title', 'directors_y', 
+    'genre', 'box_office', 'mpaa', 'runtime',  'studio', 'writer', 'full_synop', 
+    'all_reviewers_average', 'user_rating', 'year']]
+
+    
+    ratings.user_rating = ratings.user_rating.astype(str)
+    ratings.user_rating = ratings.user_rating.apply(lambda x: x.split('/')[0])
+    ratings.all_reviewers_average = ratings.all_reviewers_average.astype(str)
+    ratings.all_reviewers_average = ratings.all_reviewers_average.apply(lambda x: x.split('/')[0])
+    ratings['genre1'] = ratings['genre'].str.split(', ').str[0]
+    ratings['genre2'] = ratings['genre'].str.split(', ').str[1]
+    ratings['genre3'] = ratings['genre'].str.split(', ').str[2]
+    ratings['director1'] = ratings['directors_y'].str.split(', ').str[0]
+    ratings['director2'] = ratings['directors_y'].str.split(', ').str[1]
+    ratings['writer1'] = ratings['writer'].str.split(', ').str[0]
+    ratings['writer2'] = ratings['writer'].str.split(', ').str[1]
+    ratings['writer3'] = ratings['writer'].str.split(', ').str[2]
+    ratings['studio'] = ratings['studio'].str.split(', ').str[0]
+    ratings['mpaa'] = ratings['mpaa'].str.split(' ').str[0]
+    ratings.box_office = ratings.box_office.fillna(-1)
+    ratings.box_office = ratings.box_office.astype(str).apply(lambda x: x.replace('$', '').replace(',', '')).astype(int)
+
+    ratings = ratings[['tconst', 'averageRating', 'title',
+       'box_office', 'mpaa', 'runtime', 'studio', 'full_synop',
+       'all_reviewers_average', 'user_rating', 'year', 'genre1', 'genre2',
+       'genre3', 'director1', 'director2', 'writer1', 'writer2', 'writer3']]
+
+    ratings.columns = ['movie_id', 'imdb_score', 'title',
+       'box_office', 'mpaa', 'runtime', 'studio', 'full_synop',
+       'critics_score', 'audience_score', 'year', 'genre1', 'genre2',
+       'genre3', 'director1', 'director2', 'writer1', 'writer2', 'writer3']
+
+    ratings.to_csv('cleaned_matches.csv', index = False)
+
+    return ratings
 
 def find_movies(ui_dict):
     '''
@@ -17,15 +59,15 @@ def find_movies(ui_dict):
         return ([], [])
 
     else:
-        connection = sqlite3.connect(DATABASE_FILENAME)
+        connection = sqlite3.connect('complete.db')
         c = connection.cursor()
+        connection.create_function("fuzz", 2, fuzz.ratio)
         params = get_where_params(ui_dict)[1]
-        query = generate_query(ui_dict)
+        query = get_query(ui_dict)
         r = c.execute(query, params)
-        header = get_header(c)
         movies = r.fetchall() 
         connection.close()
-        return header, movies
+        return movies
 
 def get_query(ui_dict):
     QUERY = get_select(ui_dict) + get_from(ui_dict) + \
@@ -34,26 +76,31 @@ def get_query(ui_dict):
     return QUERY
 
 def get_select(ui_dict):
-    query_SELECT = "SELECT ratings.top_actors, ratings.all_reviewers_average, " +
-                    "ratings.user_rating, ratings.box_office, " +  
-                    "ratings.poster_url, ratings.short_syn, " + 
-                    "ratings.runtime, oscars.category, " + 
-                    "acting_nominees.category"
+    current_SELECT = ['ratings.title', 'ratings.critics_score', 'ratings.audience_score', 'ratings.box_office']
+    actual_SELECT = ['ratings.title', 'ratings.genre1', 'ratings.genre2', 'ratings.genre3', 'ratings.director1',
+                     'ratings.writer1', 'ratings.top_actors', 'ratings.critics_score', 'ratings.audience_score', 
+                     'ratings.imdb_score', 'ratings.box_office', 'ratings.poster_url', 'ratings.short_syn', 
+                     'ratings.runtime']
+    query_SELECT = 'SELECT DISTINCT ' + ', '.join(current_SELECT)
+    
     return query_SELECT
 
 
 def get_from(ui_dict):
-    query_FROM = "FROM ratings JOIN principal JOIN names JOIN awards JOIN acting_nominees" +
-           "ON ratings.tconst = principal.tconst AND principal.nconst = names.nconst " +
-           "AND ratings.tconst = oscars.tconst AND principal.nconst = acting_nominees.nconst " +
+    query_FROM = " FROM ratings JOIN principal JOIN names ON ratings.movie_id = principal.movie_id AND principal.name_id = names.name_id"
+    '''
+    "JOIN awards JOIN acting_nominees" + \
+           "ON ratings.tconst = principal.tconst AND principal.nconst = names.nconst " + \
+           "AND ratings.tconst = oscars.tconst AND principal.nconst = acting_nominees.nconst " + \
            "AND principal.tconst = acting_nominees.tconst"
+    '''
 
     return query_FROM
 
 def get_where_params(ui_dict):
-    WHERE_DICT = {"genre": "ratings.genre == ?",
-                  "actor": "(names.primaryName == ? and (principal.category = actor or principal.category = actress))",
-                  "director": "(names.primaryName == ? and principal.category = director)",
+    WHERE_DICT = {"genre": "(ratings.genre1 == ? OR ratings.genre2 == ? OR ratings.genre3 == ?)",
+                  "actor": "(fuzz(names.name, ?) >= 80 and (principal.category == 'actor' or principal.category == 'actress'))",
+                  "director": "(fuzz(names.name, ?) >= 80 and principal.category == 'director')",
                   "studio": "studio = ?",
                   "rating": "rating = ?",
                   "runtime": "runtime <= ?"}
@@ -63,21 +110,36 @@ def get_where_params(ui_dict):
     for arg in ui_dict:
         if arg in WHERE_DICT:
             WHERE.append(WHERE_DICT[arg])
-            params.append(ui_dict[arg])
+            if arg == 'genre':    
+                params += 3 * [ui_dict[arg][:]]
+            elif arg in ['actor', 'director', 'studio']:
+                params.append(ui_dict[arg].title())
+            else: 
+                params.append(ui_dict[arg])
 
     query_WHERE = " WHERE " + " AND ".join(WHERE)
 
-    return query_WHERE, PARAMS
+    return query_WHERE, params
 
 def get_orderby(ui_dict):
     ORDERBY_DICT = {"oscar_winners": "",
-                    "critics_score": "ratings.all_reviewers_average DESC",
-                    "audience_score": "ratings.user_rating DESC",
+                    "critics_score": "ratings.critics_score DESC",
+                    "audience_score": "ratings.audience_score DESC",
                     "box_office": "ratings.box_office DESC"}
     
-    query_ORDERBY = "ORDER BY " + ui_dict['order_by']
+    query_ORDERBY = "ORDER BY " + ORDERBY_DICT[ui_dict['order_by']]
 
     return query_ORDERBY
 
+TEST_0 = {'genre': 'Drama',
+          'actor': "TOM CRUISE",
+          'order_by': 'critics_score'}
 
+TEST_1 = {
+    'genre': 'Drama',
+    'actor': 'Matt Damon',
+    'studio': 'Universal',
+    'runtime': 150,
+    'mpaa': "PG-13",
+    'order_by': 'audience_score'}
 
