34d055f1e3b2ced068614c4e830da048c1fc9f58
Abdallah Aboelela <abdallahaboelela@github.com>
2019-03-04 18:58:54 -0600

    Done with model, needs testing with group

---
+++ mysite/abdallah_temp/actor_director_posters.py
@@ -0,0 +1,4 @@
+# Assume have movie_id for rotten tomatoes:
+# - Go to actor posters table:
+#     - If actor poster there, return url
+#     - Else: Find photo of first person in the actors, and return that poster url, as well as saving it in the db
