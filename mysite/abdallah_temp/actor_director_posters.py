c4bf28ad0615d13fafa9a2aad720a3ecb20d9595
Abdallah Aboelela <akaboelela@cs.uchicago.edu>
2019-03-04 20:16:20 -0600

    Done with actor_director_posters

--- mysite/abdallah_temp/actor_director_posters.py
+++ mysite/abdallah_temp/actor_director_posters.py
@@ -15,11 +15,14 @@ import urllib.request
 import bs4
 
 def run(director_name, actor_name, movie_url):
+    # THIS WOULD BE EASIER WITH NAME_ID
     connection = sqlite3.connect('../../sql_filter/updated_complete.db')
 
     c = connection.cursor()
+    actor_name = "'" + actor_name + "'"
+    director_name = "'" + director_name + "'"
 
-    s = 'SELECT poster_url FROM names WHERE person == '
+    s = 'SELECT poster_url FROM names WHERE name = '
     
     actor_s = s + actor_name + ''' AND profession LIKE '%actor%' '''
     director_s = s + director_name + ''' AND profession LIKE '%director%' '''
@@ -34,13 +37,13 @@ def run(director_name, actor_name, movie_url):
     if not actor_url and not director_url:
         director_url, actor_url = scrape(movie_url)
 
-        s = 'UPDATE table_name SET poster_url = ' + actor_url
-        s += ' WHERE person == ' + actor_name ''' AND profession LIKE '%actor%' '''
+        s = 'UPDATE names SET poster_url = ' + actor_url
+        s += ' WHERE name = ' + actor_name + ''' AND profession LIKE '%actor%' '''
 
         c.execute(s)
 
-        s = 'UPDATE table_name SET poster_url = ' + director_url
-        s += ' WHERE person == ' + director_name ''' AND profession LIKE '%director%' '''
+        s = 'UPDATE names SET poster_url = ' + director_url
+        s += ' WHERE name = ' + director_name + ''' AND profession LIKE '%director%' '''
 
         c.execute(s)
 
@@ -50,27 +53,30 @@ def run(director_name, actor_name, movie_url):
 
 
 def scrape(movie_url):
+    r = urllib.request.urlopen(movie_url)
+    html = r.read()
+    soup = bs4.BeautifulSoup(html, features = 'html5lib')
+
+    actor_url = soup.find_all('div', 
+        class_ = 'cast-item media inlineBlock')[0].find('img')['data-src']
+
+    for li in soup.find_all('li', 'meta-row clearfix'):
+        label, actual = li.find_all('div')
+
+        if label.text.strip() == 'Directed By:':
+            break
+
+    url = 'https://www.rottentomatoes.com/'
+    url += actual.find('a')['href']
+
     r = urllib.request.urlopen(url)
     html = r.read()
     soup = bs4.BeautifulSoup(html, features = 'html5lib')
 
-
-
+    director_url = soup.find('div', class_ = 'celebHeroImage')['style'][22:]
+    director_url = director_url[:len(director_url) - 2]
 
     return director_url, actor_url
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
